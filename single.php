74796072b52e488a126eb9b549a886f00acd8fe5
Zahed ALAM <mzalamfr@gmail.com>
2025-10-20 23:09:56 +0200

    Related Posts section updated. Single.php and ustom.css updated

--- single.php
+++ single.php
@@ -56,59 +56,27 @@ get_header();
                         </div>
                     </div>
                     
-                    <div class="related-posts">
-                        <h3 class="related-posts-title"><?php esc_html_e( 'Related Posts', 'modern-news-portal' ); ?></h3>
-                        <div class="related-posts-grid">
-                            <?php
-                            // Get current post categories
-                            $categories = get_the_category();
-                            if ( $categories ) {
-                                $category_ids = array();
-                                foreach ( $categories as $category ) {
-                                    $category_ids[] = $category->term_id;
-                                }
-                                
-                                // Query related posts
-                                $related_args = array(
-                                    'post_type'      => 'post',
-                                    'posts_per_page' => 3,
-                                    'post__not_in'   => array( get_the_ID() ),
-                                    'category__in'   => $category_ids,
-                                );
-                                
-                                $related_query = new WP_Query( $related_args );
-                                
-                                if ( $related_query->have_posts() ) :
-                                    while ( $related_query->have_posts() ) :
-                                        $related_query->the_post();
-                                        ?>
-                                        <div class="related-post-item">
-                                            <div class="related-post-image">
-                                                <a href="<?php the_permalink(); ?>">
-                                                    <?php if ( has_post_thumbnail() ) : ?>
-                                                        <?php the_post_thumbnail( 'modern-news-portal-featured-small' ); ?>
-                                                    <?php else : ?>
-                                                        <img src="<?php echo esc_url( get_template_directory_uri() . '/images/default-featured-small.jpg' ); ?>" alt="<?php the_title_attribute(); ?>">
-                                                    <?php endif; ?>
-                                                </a>
-                                            </div>
-                                            <h4 class="related-post-title">
-                                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                            </h4>
-                                            <div class="related-post-meta">
+                    <div class="related-post-item">
+                                            <div class="related-post-row">
+                                                <div class="related-post-image">
+                                                    <a href="<?php the_permalink(); ?>">
+                                                        <?php if ( has_post_thumbnail() ) : ?>
+                                                            <?php the_post_thumbnail( 'modern-news-portal-featured-small' ); ?>
+                                                        <?php else : ?>
+                                                            <img src="<?php echo esc_url( get_template_directory_uri() . '/images/default-featured-small.jpg' ); ?>" alt="<?php the_title_attribute(); ?>">
+                                                        <?php endif; ?>
+                                                    </a>
+                                                </div>
+
+                                                <div class="related-post-title">
+                                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                                    <div class="related-post-meta">
                                                 <?php echo modern_news_portal_get_date(); ?>
                                             </div>
+                                                </div>
+                                                
+                                            </div>
                                         </div>
-                                        <?php
-                                    endwhile;
-                                    wp_reset_postdata();
-                                else :
-                                    esc_html_e( 'No related posts found.', 'modern-news-portal' );
-                                endif;
-                            }
-                            ?>
-                        </div>
-                    </div>
                     
                     <?php
                     // If comments are open or we have at least one comment, load up the comment template.
